1685d66e0db1e60786ff5797046c7ae5f662c687
gourav <gauravswain@gmail.com>
2024-01-22 13:25:59 +0000

    removed extras

--- jwtSecretGenerator.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function generateJwtSecret(length = 64) {
-    return crypto.randomBytes(length).toString('hex');
-}
-
-const jwtSecret = generateJwtSecret();
-console.log('JWT Secret:', jwtSecret);
